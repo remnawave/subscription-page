aabbe1c8c85121444f3348ef3667f197262f66d1
kastov <kastov@gog.sh>
2025-03-13 15:51:04 +0300

    refactor: update API client to use imroc/req, remove unused compression utility, and clean up response handling

--- server/api/client.go
+++ server/api/client.go
@@ -4,40 +4,54 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+
+	"github.com/imroc/req/v3"
 )
 
 type Client struct {
 	domain string
-	client *http.Client
+	client *req.Client
 }
 
 func NewClient(domain string) *Client {
+	client := req.C().
+		SetUserAgent("req").
+		DisableDebugLog()
+		// EnableDumpAll().
+		// EnableDebugLog()
+	
 	return &Client{
 		domain: domain,
-		client: &http.Client{
-			Transport: &http.Transport{},
-		},
+		client: client,
 	}
 }
 
-func (c *Client) FetchAPI(shortId string, headers http.Header) (*http.Response, error) {
-	url := fmt.Sprintf("https://%s/api/sub/%s", c.domain, shortId)
+func (c *Client) FetchAPI(path string, headers http.Header) (*req.Response, error) {
+	var url string
+	if path == "" {
+		return nil, fmt.Errorf("error creating request")
+	} else {
+		url = fmt.Sprintf("https://%s/api/sub/%s", c.domain, path)
+	}
 
 	slog.Debug("Fetching API", "url", url)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %w", err)
+	
+	request := c.client.R()
+	
+	if userAgent := headers.Get("User-Agent"); userAgent != "" {
+		request.SetHeader("User-Agent", userAgent)
+	}
+	
+	if accept := headers.Get("Accept"); accept != "" {
+		request.SetHeader("Accept", accept)
 	}
 
-	req.Header.Set("User-Agent", headers.Get("User-Agent"))
-	req.Header.Set("Accept", headers.Get("Accept"))
-	req.Header.Add("Accept-Encoding", "gzip")
-
-	resp, err := c.client.Do(req)
+	
+	resp, err := request.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("error making request: %w", err)
 	}
-
+	
 	return resp, nil
-}
+} 
--- server/handlers/subscription.go
+++ server/handlers/subscription.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"compress/gzip"
-	"github.com/gofiber/fiber/v2"
-	"io"
 	"log/slog"
-	"strings"
 	"subscription-page-template/server/api"
 	"subscription-page-template/server/utils"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type SubscriptionHandler struct {
@@ -22,6 +20,8 @@ func NewSubscriptionHandler(apiClient *api.Client) *SubscriptionHandler {
 
 func (h *SubscriptionHandler) HandleSubscription(c *fiber.Ctx) error {
 	shortId := c.Params("shortId")
+
+
 	if shortId == "" {
 		return c.Status(fiber.StatusBadRequest).SendString("Bad request.")
 	}
@@ -36,53 +36,24 @@ func (h *SubscriptionHandler) HandleSubscription(c *fiber.Ctx) error {
 		slog.Error("Error fetching API", "error", err)
 		return c.Status(fiber.StatusInternalServerError).SendString("Request error.")
 	}
-	defer resp.Body.Close()
-
-	var reader io.ReadCloser
-	switch resp.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err = gzip.NewReader(resp.Body)
-		if err != nil {
-			panic(err)
-		}
-		defer reader.Close()
-	default:
-		reader = resp.Body
-	}
-
-	body, err := io.ReadAll(reader)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString("Read response error.")
-	}
 
 	userAgent := c.Get("User-Agent")
+	isBrowser := utils.IsBrowser(userAgent)
+	
 
-	if utils.IsBrowser(userAgent) {
+	body := resp.Bytes()
+	
+	if isBrowser {
 		return c.Render("./dist/index.html", fiber.Map{
 			"Data": string(body),
 		})
 	}
-
+	
 	for name, values := range resp.Header {
 		for _, value := range values {
 			c.Set(name, value)
 		}
 	}
-
-	filteredHeaders := []string{
-		"Profile-Title",
-		"Profile-Update-Interval",
-		"Subscription-Userinfo",
-		"Profile-Web-Page-Url",
-		"Content-Disposition",
-	}
-
-	for _, header := range filteredHeaders {
-		if values, found := resp.Header[header]; found {
-			c.Set(header, strings.Join(values, ","))
-		}
-	}
-
-	c.Set("Content-Encoding", "application/gzip")
+	
 	return c.Status(resp.StatusCode).Send(body)
 }

ed8c6503bf4f1ad13dec8494c8cf90177a03c0cd
kastov <kastov@gog.sh>
2025-12-15 05:01:35 +0300

    chore: add entrypoint script to Dockerfile and update docker-entrypoint.sh

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,7 +3,6 @@
 echo "Starting entrypoint script..."
 export INTERNAL_JWT_SECRET=$(head -c 64 /dev/urandom | xxd -p)
 
-echo "Starting pm2..."
-pm2-runtime start ecosystem.config.js --env production
 
 echo "Entrypoint script completed."
+exec "$@"
